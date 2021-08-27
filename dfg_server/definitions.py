d353493bdcd81a4a0fce8fd6b68d7a8bbcfa0364
Valerius Mattfeld <10323945+valerius21@users.noreply.github.com>
2021-08-27 14:07:49 +0200

    refactor: configpath in dev mode

--- dfg_server/definitions.py
+++ dfg_server/definitions.py
@@ -2,5 +2,7 @@ import os
 
 DEFINITIONS_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.join(DEFINITIONS_DIR, '..')
-
 CONFIG_PATH = os.path.join(ROOT_DIR, './etc/config.yml')
+
+if not os.environ['DFG_PRODUCTION']:
+    CONFIG_PATH = os.path.join(os.environ['HOME'], '/.dfg', 'config.yml')
