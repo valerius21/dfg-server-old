220bf705193a761a411879188c777a2dddf34707
Valerius Mattfeld <10323945+valerius21@users.noreply.github.com>
2021-09-26 00:43:54 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from setuptools import setup
 
 setup(
     name='dfg_server',
-    version='0.1.0',
+    version='0.0.1',
     description='The DFG Project Server Middleware',
     url='https://github.com/valerius21/dfg-server',
     author='Valerius Mattfeld',
     author_email='valerius.mattfeld@uni-goettingen.de',
     license='MIT',
-    packages=['dfg_server'],
     install_requires=[
         'numpy',
         'fastapi',
@@ -24,4 +23,6 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3.8',
     ],
+    py_modules=['dfg_server'],
+    package_dir={'': 'dfg_server'},
 )
